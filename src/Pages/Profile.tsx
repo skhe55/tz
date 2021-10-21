dddd7a2bbba171d0d169cb199d12f71ed3034e27
skhe55 <flashback2019@mail.ru>
2021-10-21 17:43:26 +0700

    exact login from redux store state, using a selector function, output login in h2 tags, added button to return on home page(/)

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom';
+import { RootState } from '../Store'
+import "./styles/Profile.scss";
 
 const Profile = () => {
+    const login = useSelector((state: RootState) => state.login);
     return (
-        <div>
-            Profile
+        <div className="bodyContainer">
+            <div className="bodyFlexContainer">
+                <h1>Welcome {login}</h1>
+                <NavLink className="button_logout" exact to="/">Logout</NavLink>
+            </div>
         </div>
     )
 }
