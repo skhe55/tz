e43d0012c267ee35192b88e0394978d48b1304bd
skhe55 <flashback2019@mail.ru>
2021-10-21 15:27:35 +0700

    added navigation btw pages(react-router-dom)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import './Styles/global.scss';
 
 export const App: React.FC<{}> = () => {
     return (
-        <>
-            Test
-        </>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/Profile" component={Profile} />
+            </Switch>
+        </Router>
     );
 };
